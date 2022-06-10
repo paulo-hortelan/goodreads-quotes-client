cf95075024a01521a2b8b699b21c89ac4e357745
paulo <paulohincar@gmail.com>
2022-06-09 21:03:38 -0300

    Add Button component and test

---
+++ src/components/button/Button.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+    background: #3d4839;
+    color: #fff;
+    border: none;
+    border-radius: 10px;
+    font-size: 1.5em;
+    padding: 10px 20px;
+    cursor: pointer;
+    box-shadow: #333 3px 3px;
+
+    &:hover {
+        background: #729466;
+    }
+`;
